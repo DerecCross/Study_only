7db442cb45e1e75100a9737c3096d7ec59924edb
Vladimir Moskaliuk <vovik.ru.2010@mail.ru>
2025-08-06 22:31:10 +0300

    Array

---
+++ array.php
@@ -0,0 +1,26 @@
+<?php
+$array = [
+    [
+    'id' => 1,
+    'name' => 'Product1',
+    'price' => 3.99,
+    'sort' => 100,
+    'quantity' => 2000,
+    ],
+    [
+        'id' => 2,
+        'name' => 'Product2',
+        'price' => 3.99,
+        'sort' => 13,
+        'quantity' => 4,
+    ],
+    [
+        'id' => 3,
+        'name' => 'Product3',
+        'price' => 3.99,
+        'sort' => 15,
+        'quantity' => 5,
+    ],
+];
+
+
