76bace915d6abf6fcf74f1b05b1d4bb5f512e2cd
Vladimir Moskaliuk <vovik.ru.2010@mail.ru>
2025-08-07 23:07:09 +0300

    Добавление новых эллементов в массив вручную и через функцию.

--- array.php
+++ array.php
@@ -22,5 +22,35 @@ $array = [
         'quantity' => 5,
     ],
 ];
+//добавление нового элемента массива
+$array[] = [
+    'id' => 8,
+    'name' => 'Product9',
+    'price' => 3.99,
+    'sort' => 100,
+    'quantity' => 2000,
+];
+//добавление нового элемента массива
+$array[] = [
+    'id' => 10,
+    'name' => 'Product10',
+    'price' => 1,
+    'sort' => 1,
+    'quantity' => 2,
+];
 
+//добавление нового элемента массива через функцию
+function newProduct(&$array, $id, $name, $price, $sort = 100, $quantity = 0) {
+    $array[] = [
+        'id' => $id,
+        'name' => $name,
+        'price' => $price,
+        'sort' => $sort,
+        'quantity' => $quantity
+    ];
+}
 
+newProduct($array, 15, 'Новый', 100, 10, 5000 );
+newProduct($array, 16, 'Новый2 ', 90);
+
+print_r($array);
